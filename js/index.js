a8db3fb4082b28b10694b69c3757b0dcecd8a575
Dani <97204979+dclarke35@users.noreply.github.com>
2023-09-25 21:24:50 -0600

    Update index.js
    placeholder attributes - changes made via js in index.js as comments

--- js/index.js
+++ js/index.js
@@ -4,16 +4,25 @@ const inputFirstName = document.querySelector('#firstName')
 const inputLastName = document.querySelector('#lastName')
 const displayFullName = document.querySelector('#displayFullName')
 
+/* 
+    Just incase this was the intended way to change the placeholder attributes
+    inputFirstName.placeholder = "John"
+    inputLastName.placeholder = "Hancock"
+
+    displayFullName.value = inputFirstName.placeholder + " " + inputLastName.placeholder
+*/
+
 updateAccountProfileBtn.addEventListener('click', onUpdateAccountProfile)
 function onUpdateAccountProfile(e){
 
     e.preventDefault()
-    console.log(`first name: ${inputFirstName.value}`)
-    console.log(`last name: ${inputLastName.value}`)
+    
+    // console.log(`first name: ${inputFirstName.value}`)
+    // console.log(`last name: ${inputLastName.value}`)
 
     const fullName = inputFirstName.value + " " + inputLastName.value
 
-    console.log(`full name: ${fullName}`)
+    // console.log(`full name: ${fullName}`)
 
     displayFullName.value = fullName
 
